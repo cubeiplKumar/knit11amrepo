3df4cf7415c6620f3fa0da8f9fdeb22de5bc64fe
cubeiplkumar <emailanilkumarm@gmail.com>
2021-09-28 12:14:33 +0530

    new changes using stash feature

--- helloworld.java
+++ helloworld.java
@@ -10,4 +10,9 @@ public class HelloWorld {
         int c = a/b; //  int c = 4/2 which gets calculated as 2 
         System.out.println("c:"+c); // c: 2
     }
+	public static void main (String[] args){
+    DemoClass.sampleMethod(4,2); 
+	System.out.println("GitHub Advanced Commands!!!");
+ }
+
 }
