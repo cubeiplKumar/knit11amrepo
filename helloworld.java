f55a48af9153eede6afe78a4f511c948ca201dbd
cubeiplkumar <emailanilkumarm@gmail.com>
2021-09-28 12:00:13 +0530

    second commit changes done by dev3

--- helloworld.java
+++ helloworld.java
@@ -5,6 +5,9 @@ public class HelloWorld {
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
 	System.out.println ("Hello World!");
-	System.out.println"Github Advanced Commands");
+	System.out.println("Github Advanced Commands");
+	 System.out.println("Hello, this is sample method");
+        int c = a/b; //  int c = 4/2 which gets calculated as 2 
+        System.out.println("c:"+c); // c: 2
     }
 }
