60ea2eedaacc3ee50021cf00c2d02c1f30582807
cubeiplkumar <emailanilkumarm@gmail.com>
2021-09-28 12:18:03 +0530

    new changes

--- helloworld.java
+++ helloworld.java
@@ -5,14 +5,10 @@ public class HelloWorld {
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
 	System.out.println ("Hello World!");
-<<<<<<< HEAD
 	System.out.println("Github Advanced Commands");
 	 System.out.println("Hello, this is sample method");
         int c = a/b; //  int c = 4/2 which gets calculated as 2 
         System.out.println("c:"+c); // c: 2
-=======
-	System.out.println("Working on Git branches and Merge Conflicts");
->>>>>>> 3e4023e0370172cb01324cd927c0a492ab044886
     }
 	public static void main (String[] args){
     DemoClass.sampleMethod(4,2); 
