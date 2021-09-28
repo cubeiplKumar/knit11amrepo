aab9a8f8f3dd9303cf22454c9da1f0b0b6084f2a
cubeiplkumar <emailanilkumarm@gmail.com>
2021-09-28 11:45:37 +0530

    Feature301 file has been modified by mahesh

--- helloworld.java
+++ helloworld.java
@@ -6,5 +6,11 @@ public class HelloWorld {
     public static void main (String[] args) {
 	System.out.println ("Hello World!");
 	System.out.println("Working on Git branches and Merge Conflicts");
-    }
+	     }
+	public static void sampleMethod(int a, int b) throws ArithmeticException{
+    System.out.println("Hello, this is sample method");
+	int c = a/b; //  int c = 4/2 which gets calculated as 2 
+        System.out.println("c:"+c); // c: 2
+   } 
+		
 }
